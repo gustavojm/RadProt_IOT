85b5429320ebd0896190429700f0de33282ea1b1
Gustavo Malano <gmalano@na-sa.com.ar>
2025-02-24 16:52:11 -0300

    changed TCP_MSS to default 1460

--- inc/lwipopts.h
+++ inc/lwipopts.h
@@ -22,7 +22,7 @@
 #define LWIP_RAW 1
 #define TCP_WND (8 * TCP_MSS)
 //#define TCP_MSS 1460
-#define TCP_MSS 1440
+#define TCP_MSS 1460  // if the MTU is 1,500 bytes, then the MSS will be 1,460 bytes. 
 #define TCP_SND_BUF (8 * TCP_MSS)
 #define TCP_SND_QUEUELEN ((4 * (TCP_SND_BUF) + (TCP_MSS - 1)) / (TCP_MSS))
 #define LWIP_NETIF_STATUS_CALLBACK 1
